f6327443715180ef656ec087f64c1b43bb32e778
Nicolas Ducoulombier <nicolas@ldd.fr>
2015-06-22 13:19:02 -0500

    Fixing attendance calculation and attendance presentation with group management - refs BT#9425

--- main/attendance/attendance_controller.php
+++ main/attendance/attendance_controller.php
@@ -265,8 +265,7 @@ class AttendanceController
         $data = array();
         $data['attendance_id'] = $attendance_id;
         $groupId = isset($_REQUEST['group_id']) ? $_REQUEST['group_id'] : null;
-        $data['users_in_course'] = $attendance->get_users_rel_course($attendance_id, $groupId);
-
+        $data['users_in_course'] = $attendance->get_users_rel_course($attendance_id, 0, $groupId);
         $filter_type = 'today';
         if (!empty($_REQUEST['filter'])) {
             $filter_type = $_REQUEST['filter'];
@@ -279,7 +278,7 @@ class AttendanceController
 
         if ($edit == true) {
             if (api_is_allowed_to_edit(null, true) || $isDrhOfCourse) {
-                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id);
+                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id, 0, $groupId);
             }
         } else {
             if (!empty($student_id)) {
@@ -292,9 +291,9 @@ class AttendanceController
                 api_is_coach(api_get_session_id(), api_get_course_id()) ||
                 $isDrhOfCourse
             ) {
-                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id);
+                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id, 0, $groupId);
             } else {
-                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id, $user_id);
+                $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id, $user_id, $groupId);
             }
 
             $data['faults']  = $attendance->get_faults_of_user($user_id, $attendance_id);
@@ -331,12 +330,12 @@ class AttendanceController
                 $my_calendar_id,
                 $groupId
             );
-            $data['attendant_calendar_all'] = $attendance->get_attendance_calendar($attendance_id);
-            $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id);
+            $data['attendant_calendar_all'] = $attendance->get_attendance_calendar($attendance_id, 'all', null, $groupId);
+            $data['users_presence'] = $attendance->get_users_attendance_sheet($attendance_id, 0, $groupId);
             $data['next_attendance_calendar_id'] = $attendance->get_next_attendance_calendar_id($attendance_id);
             $data['next_attendance_calendar_datetime'] = $attendance->get_next_attendance_calendar_datetime($attendance_id);
         } else {
-            $data['attendant_calendar_all'] = $attendance->get_attendance_calendar($attendance_id);
+            $data['attendant_calendar_all'] = $attendance->get_attendance_calendar($attendance_id, 'all', null, $groupId);
             $data['attendant_calendar'] = $attendance->get_attendance_calendar($attendance_id, $filter_type, null, $groupId);
         }
 
